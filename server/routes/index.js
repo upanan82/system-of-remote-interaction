c2cffb5dec9fe014de525e57606cad5c6311ff91
Dmytro Onyshchenko <dmytro.onyshchenko@synapse.com>
2018-03-10 01:32:32 +0200

    done config files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,18 @@
-import express from 'express';
 import ssr from './ssr';
+
+const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(favicon('../client/res/images/favicon.ico'));
 app.use(express.static('public'));
 
 app.use('/*', ssr);
 
 app.listen(3000, () => {
-  console.log('Hello World listening on port 3000!');
+    console.log('Hello World listening on port 3000!');
 });
--- webpack/server.prod.js
+++ webpack/server.prod.js
@@ -34,7 +34,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|gif|ico)$/,
                 use: [
                     {
                         loader: 'file-loader',
